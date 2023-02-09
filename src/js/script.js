33130092b9c5189b106a50aeccbef791a664f190
Spooked_Potato <arturjddias@hotmail.com>
2023-02-09 00:59:01 +0000

    Created Thank you Page

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,26 @@ $(function () {
     $(this).load(file);
   });
 });
+
+function initContactForm() {
+  const form = document.getElementById("contactForm");
+
+  if (!form) return;
+
+  form.addEventListener("submit", () => {
+    alert("Message was send successfully");
+  });
+}
+initContactForm();
+
+function initThankYou() {
+  goBack = document.getElementById("btn");
+
+  if (!goBack) return;
+
+  goBack.addEventListener("click", test);
+  function test() {
+    window.location.href = "index.html";
+  }
+}
+initThankYou();
